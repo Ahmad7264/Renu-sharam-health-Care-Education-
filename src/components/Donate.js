f5e7ba5c6d294a4c26949acda6591d1b0e273eee
Ahmad7264 <dilshadahmad286@gmail.com>
2024-04-29 19:58:17 +0530

    Changes in Form

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 import image1 from "../assets/img1.png";
 import image2 from "../assets/img2.png";
 import image3 from "../assets/img4.png";
@@ -7,52 +9,65 @@ import image5 from "../assets/mainimg2.png";
 import image6 from "../assets/education.png";
 import logo1 from "../assets/logo-give.png";
 import donateImg from "../assets/donateimg.png";
-import donationForm from "../assets/donationform.png";
-import donateimg2 from "../assets/donateform.png";
-import donateimg3 from "../assets/donateformfinal.png";
+import donateForm from "../assets/donate.png";
+
 const Donate = () => {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    const regex = /^\d{10}$/;
+    const isValidPhoneNumber = regex.test(value);
+    setPhoneNumber(value);
+    setIsValid(isValidPhoneNumber);
+  };
+
   return (
     <>
-      <div
-        // variants={fadeIn("up", 0.2)}
-        // initial="hidden"
-        // whileInView={"show"}
-        // viewport={{ once: false, amount: 0.6 }}
-        className="min-h-screen flex flex-col justify-center  sm:px-14 lg:px-32 px-5 text-white bg-[url('assets/main3.png')] bg-no-repeat bg-cover opacity-90"
+      <motion.div
+        variants={fadeIn("down", 0.3)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.6 }}
+        className="min-h-screen flex flex-col justify-center  sm:px-14 lg:px-32 px-5 text-white bg-[url('assets/tabimage1.png')] bg-no-repeat bg-cover opacity-90 md:bg-[url(assets/main3.png)]"
       >
-        <div
-          // variants={fadeIn("left", 0.2)}
-          // initial="hidden"
-          // whileInView={"show"}
-          // viewport={{ once: false, amount: 0.6 }}
-          className=" w-full lg:w-4/5 space-y-5 "
-        >
-          {/* <h1 className="text-6xl font-bold mb-4 text-bPrimary font-serif md:w-3/4  leading-snug">
-          Renu Health <span className="text-dGrey">Care</span>
-          
-        </h1> */}
-          <img src={logo1} alt="imag" />
-          <p className="text-bold font-bold hover:text-bPrimary  font-serif ">
-            If your project grows in size, this option is available.<br></br>
-            This method has the trade-off that it takes<br></br>a long time to
-            install the package.<br></br>
-            If your project grows in size, this option is <br></br>available.
-            This method time to install the package. If your project<br></br>
-            grows in size, this option is available. This method has the
-            <br></br> trade-off that it takes a long time to install the
-            package.
-          </p>
+        <div className=" w-full lg:w-4/5 content-center space-y-5 ">
+          <motion.img
+            variants={fadeIn("down", 0.8)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.6 }}
+            src={logo1}
+            alt="img"
+          />
+          <div className=" flex ">
+            <p className=" font-bold w-3/4 text-start mt-10 hover:text-bPrimary hidden md:block tracking-widest ">
+              With your contribution, we can helping children access education,
+              providing healthcare to communities in need, supporting disaster
+              relief efforts..
+            </p>
+          </div>
         </div>
-      </div>
+      </motion.div>
 
-      {/* card items */}
+      {/* header Section End */}
+
+      {/* card items  start*/}
+
       <div className=" max-w-full mx-auto my-5">
         <h1 className=" text-4xl text-center font-bold text-dGrey hover:text-bPrimary tracking-widest font-serif">
-          Donate to the Ngo
+          Donate to the NGO
         </h1>
       </div>
-      <div className=" max-w-7xl mx-auto grid lg:grid-cols-3 md:grid-cols-2 gap-5 px-8">
-        <div className="text-center shadow-xl mb-11 rounded-xl ">
+      <motion.div className=" max-w-7xl mx-auto grid lg:grid-cols-3 md:grid-cols-2 gap-5 px-8">
+        <motion.div
+          variants={fadeIn("down", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="text-center shadow-xl mb-11 rounded-xl "
+        >
           <div className=" overflow-hidden">
             <img
               src={image1}
@@ -60,14 +75,20 @@ const Donate = () => {
               alt=""
             />
           </div>
-          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
-            Give for Poor children
+          <h2 className=" bg-amber-600 cursor-pointer font-bold tracking-widest py-2">
+            Donate for Poor children
           </h2>
           <p className="py-2">
             Rapidly build modern websites without ever leaving your HTML.
           </p>
-        </div>
-        <div className="text-center shadow-xl mb-11 rounded-xl ">
+        </motion.div>
+        <motion.div
+          variants={fadeIn("down", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="text-center shadow-xl mb-11 rounded-xl "
+        >
           <div className=" overflow-hidden">
             <img
               src={image2}
@@ -75,14 +96,20 @@ const Donate = () => {
               alt=""
             />
           </div>
-          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
-            Give for Better LIfe
+          <h2 className=" bg-amber-600 cursor-pointer font-bold tracking-widest py-2">
+            Donate for Better LIfe
           </h2>
           <p className="py-2">
             Rapidly build modern websites without ever leaving your HTML.
           </p>
-        </div>
-        <div className="text-center shadow-xl mb-11 rounded-xl ">
+        </motion.div>
+        <motion.div
+          variants={fadeIn("down", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="text-center shadow-xl mb-11 rounded-xl "
+        >
           <div className=" overflow-hidden">
             <img
               src={image3}
@@ -90,14 +117,20 @@ const Donate = () => {
               alt=""
             />
           </div>
-          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
-            Give for Home
+          <h2 className=" bg-amber-600 cursor-pointer font-bold tracking-widest py-2">
+            Donate for Better Home
           </h2>
           <p className="py-2">
             Rapidly build modern websites without ever leaving your HTML.
           </p>
-        </div>
-        <div className="text-center shadow-xl mb-11 rounded-xl ">
+        </motion.div>
+        <motion.div
+          variants={fadeIn("down", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="text-center shadow-xl mb-11 rounded-xl "
+        >
           <div className=" overflow-hidden">
             <img
               src={image4}
@@ -105,14 +138,20 @@ const Donate = () => {
               alt=""
             />
           </div>
-          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
-            Give for Better Food
+          <h2 className=" bg-amber-600 cursor-pointer font-bold tracking-widest py-2">
+            Donate for Better Food
           </h2>
           <p className="py-2">
             Rapidly build modern websites without ever leaving your HTML.
           </p>
-        </div>
-        <div className="text-center shadow-xl mb-11 rounded-xl ">
+        </motion.div>
+        <motion.div
+          variants={fadeIn("down", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="text-center shadow-xl mb-11 rounded-xl "
+        >
           <div className=" overflow-hidden">
             <img
               src={image5}
@@ -120,14 +159,20 @@ const Donate = () => {
               alt=""
             />
           </div>
-          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
-            Give for Better Food
+          <h2 className=" bg-amber-600 cursor-pointer font-bold tracking-widest py-2">
+            Donate for Better Food
           </h2>
           <p className="py-2">
             Rapidly build modern websites without ever leaving your HTML.
           </p>
-        </div>
-        <div className="text-center shadow-xl mb-11 rounded-xl ">
+        </motion.div>
+        <motion.div
+          variants={fadeIn("down", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="text-center shadow-xl mb-11 rounded-xl "
+        >
           <div className=" overflow-hidden">
             <img
               src={image6}
@@ -135,73 +180,121 @@ const Donate = () => {
               alt=""
             />
           </div>
-          <h2 className=" bg-amber-700 cursor-pointer font-bold tracking-widest py-2">
-            Give for Better Education
+          <h2 className=" bg-amber-600 cursor-pointer font-bold tracking-widest py-2">
+            Donate for Better Education
           </h2>
           <p className="py-2">
             Rapidly build modern websites without ever leaving your HTML.
           </p>
-        </div>
-      </div>
-      <div className="w-full h-screen flex items-start">
-        <div className="relative w-1/2 h-full flex flex-col ">
-          <img
-            src={donateimg3}
-            className="w-full h-full object-cover"
-            alt=""
-          />
-        </div>
-        <div className="w-1/2 h-full bg-[url('assets/img2.png')] bg-no-repeat bg-cover opacity-90 flex flex-col p-10">
-          <h1 className="text-4xl text-bPrimary text-center   font-bold mb-4">
-            Renu Sharma Health Care
-          </h1>
-          <div className="w-full flex flex-col">
-            <div className="w-full flex flex-col mb-1">
-              <h2 className="text-2xl text-white text-center font-serif font-semibold mb-4">
-                Support Us
-              </h2>
-              <p className=" text-base text-white text-center font-serif mb-2">
-                Welcome Back! Please enter your details.
-              </p>
-            </div>
-            <div className="w-full flex flex-col">
+        </motion.div>
+      </motion.div>
+
+      {/* Card Section End */}
+
+      {/* Donation form starts */}
+      <motion.div className=" w-full flex items-center justify-center min-h-screen bg-gray-100">
+        <motion.div className="relative flex flex-col m-4 space-y-2 bg-white  shadow-2xl rounded-2xl md:flex-row md:space-y-0">
+          <motion.div
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.6 }}
+            className="flex flex-col justify-center mt-0 72634mb-16 p-2 md:p-14 "
+          >
+            <span className="mb-4 text-4xl font-bold text-bPrimary">
+              Renu Sharma HealthCare
+            </span>
+            <span className="font-bold text-2xl text-center text-gray-1000 mb-4 tracking-widest">
+              Support Us!
+            </span>
+            <span className="font-light text-gray-400 text-center mb-1">
+              Hey! Enter your details here!
+            </span>
+            <div className="py-3">
+              <span className="mb-2 text-md  tracking-wider">Name</span>
               <input
                 type="text"
-                placeholder="Name"
-                className="w-full text-black rounded-3xl py-4 my-3 border-b  border-black outline-none focus:outline-none"
-              />
-              <input
-                type="email"
-                placeholder="Email"
-                className="w-full text-black rounded-3xl py-4 my-3 border-b  border-black outline-none focus:outline-none"
-              />
-              <input
-                type="number"
-                placeholder="Number"
-                className="w-full text-black rounded-3xl py-4 my-3 border-b  border-black outline-none focus:outline-none"
+                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+                name="Name"
+                id="name"
+                placeholder="Full Name"
               />
             </div>
-            <input
-              type="number"
-              placeholder="Amount"
-              className="w-full text-black rounded-3xl py-4 my-3 border-b  border-black outline-none focus:outline-none"
-            />
-          </div>
-          <div className="w-full flex flex-col items-center my-4">
-            <button className="w-1/2 text-2xl text-white my-2 bg-black rounded-lg p-4 text-center flex item-center justify-center font-serif font-bold tracking-widest">
-              Donate Now
+            <div className="py-3">
+              <span className="mb-2 text-md tracking-wider">Email</span>
+              <input
+                type="text"
+                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+                name="Email"
+                id="email"
+                placeholder="Email Address"
+              />
+            </div>
+            <div className="py-3">
+              <span className="mb-2 text-md tracking-wider ">
+                Mobile Number
+              </span>
+              <input
+                type="tel"
+                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+                name="Mobile Number"
+                id="Mobile"
+                placeholder="Mobile Number"
+                value={phoneNumber}
+                onChange={handleChange}
+              />
+              {!isValid && (
+                <p style={{ color: "red" }}>
+                  Please enter a valid 10-digit phone number.
+                </p>
+              )}
+            </div>
+            <div className="py-3">
+              <span className="mb-2 text-md tracking-wider ">Your Amount</span>
+              <input
+                type="number"
+                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+                name="Your Amount"
+                id="amount"
+                placeholder="Your Amount"
+              />
+            </div>
+            <button
+              id="button"
+              className="w-full mt-8 bg-black text-white p-3 rounded-lg  hover:bg-white hover:text-black hover:border hover:border-gray-300"
+            >
+              Donate Now!
             </button>
-          </div>
-        </div>
-      </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.6 }}
+            className="relative"
+          >
+            <img
+              src={donateForm}
+              alt="img"
+              className="w-[500px] h-[681px] hidden  rounded-r-2xl md:block object-cover"
+            />
+          </motion.div>
+        </motion.div>
+      </motion.div>
 
-      <div className="w-full">
+      <motion.div
+        variants={fadeIn("down", 0.3)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.6 }}
+        className=" w-full"
+      >
         <img
           src={donateImg}
           alt=""
           className="w-full h-1/2 mt-6 mb-6 shadow-2xl rounded-2xl"
         />
-      </div>
+      </motion.div>
     </>
   );
 };
